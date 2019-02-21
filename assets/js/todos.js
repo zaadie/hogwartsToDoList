3f8dd75d820fb6f2741de9fca9f50ed79d77ea5c
zaadie <saadiyafourie@gmail.com>
2019-02-21 14:19:11 +1100

    Function created that appends input to the bottom of the todo List

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,25 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
+    // Cross or Un-cross list items 
     $(this).toggleClass("completed");
 });
 
 
-//Click on X to delete Todp
+//Click on X to delete Todo
 $("span").click(function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     })
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //extract the text value
+        var todoText = $(this).val();
+        //create a new li add to ul
+        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+        // clear text value in input
+    }
 });
 
 
